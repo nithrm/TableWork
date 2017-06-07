20ceee55733bbdbc39594c6b2db19b9c45f51b88
Eliot Szwajkowski <eliotpszw@gmail.com>
2017-06-07 17:00:08 -0400

    db changed.

--- server/db/db.js
+++ server/db/db.js
@@ -4,10 +4,10 @@ import path from 'path';
 import chalk from 'chalk';
 import Sequelize from 'sequelize';
 
-const memeDB = 'meme-magic';
+const memeDB = process.env.MAIN_DB_URL;
 
 console.log(chalk.yellow('Hey Rick!'));
 
-const _db = new Sequelize(memeDB, 'postgres', null, {host: 'localhost', port: 5433, logging: false, dialect: 'postgres', native: true});
+const _db = new Sequelize(memeDB);
 
 export default _db;
